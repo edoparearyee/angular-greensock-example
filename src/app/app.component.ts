b001e91d2ae00af7f264d9cc935930ba8f44c571
Ed Opare-Aryee <ed@thisissoon.com>
2018-03-16 14:52:55 +0000

    feat(universal): add server side rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, NgZone, ChangeDetectionStrategy, Inject, PLATFORM_ID } from '@angular/core';
+import { TimelineMax, Linear } from 'gsap';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  tl;
+  @ViewChild('circle') circle: ElementRef;
+
+  constructor(
+    private ngZone: NgZone,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
+
+  ngAfterViewInit() {
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.ngZone.runOutsideAngular(() => {
+        this.tl = new TimelineMax({
+          repeat: Infinity,
+          repeatDelay: 2
+        });
+
+        this.tl
+          .set(this.circle.nativeElement, { x: 0 })
+          .from(this.circle.nativeElement, 3, {
+            x: 200,
+            rotation: 180,
+            transformOrigin: 'center center',
+            ease: Linear.ease
+          })
+          .from(this.circle.nativeElement, 2, {
+            x: 0,
+            rotation: 0,
+            transformOrigin: 'center center',
+            ease: Linear.ease
+          });
+
+        this.tl.play();
+      });
+    }
+
+  }
 }
